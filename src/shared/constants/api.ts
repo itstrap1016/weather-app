1c0f2e09ead6317e9f8b6d16a4ad2300eb7cc942
이한승 <itstrap1016@gmail.com>
2025-07-22 19:18:59 +0900

    api 파일 수정

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-  BASE_URL: process.env.VITE_WEATHER_BASE_URL,
-  API_KEY: process.env.VITE_WEATHER_API_KEY,
+  BASE_URL: import.meta.env.VITE_BASE_URL,
+  API_KEY: import.meta.env.VITE_API_KEY,
   DEFAULT_PARAMS: {
     units: "metric",
     lang: "kr",
