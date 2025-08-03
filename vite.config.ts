db2b2b6d89dc759a551ddbcd472b0aeb66c6b61a
이한승 <itstrap1016@gmail.com>
2025-08-03 16:37:43 +0900

    github acttions debug 코드 추가

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@/domain": path.resolve(__dirname, "./src/domain"),
+      "@/infrastructure": path.resolve(__dirname, "./src/infrastructure"),
+      "@/application": path.resolve(__dirname, "./src/application"),
+      "@/presentation": path.resolve(__dirname, "./src/presentation"),
+      "@/shared": path.resolve(__dirname, "./src/shared"),
     },
   },
   base: process.env.NODE_ENV === "production" ? "/weather-app/" : "/",
